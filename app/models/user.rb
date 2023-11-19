ee95faed0bac432fe1013a4addb1a8c73dc60ed7
mamiprogramming <mamiprogramming@gmail.com>
2023-11-19 20:55:28 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :familyname_kanji, presence: true
+  validates :firstname_kanji, presence: true
+  validates :familyname_katakanai, presence: true
+  validates :firstname_katakana, presence: true
+  validates :birthday, presence: true
 end
