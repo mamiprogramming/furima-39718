ab584e2c0fb7ab4bf7969e7a21f545dc514a4552
mamiprogramming <mamiprogramming@gmail.com>
2023-11-20 23:14:54 +0900

    新規登録ログイン画面遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'items/index'
   devise_for :users
+  get 'items/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "items#index"
+  resources :items, only: [:index, :new]
 end
