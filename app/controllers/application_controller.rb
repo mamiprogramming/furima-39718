4354ca9c605aee2a029fd9fc1592d9c0c1e56d07
mamiprogramming <mamiprogramming@gmail.com>
2023-11-21 22:19:10 +0900

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   private
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :familyname_kanji, :firstname_kanji, :familyname_katakana, :firstname_katakana, :birthday,])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:name, :familyname_kanji, :firstname_kanji, :familyname_katakana,
+                                             :firstname_katakana, :birthday])
   end
 
   def basic_auth
