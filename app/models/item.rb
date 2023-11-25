f48a0f58a0d395d4891c35c825aaeedf2e6b3233
mamiprogramming <mamiprogramming@gmail.com>
2023-11-25 20:08:20 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-end
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :explain, presence: true
+  validates :category_id, presence: true
+  validates :situation_id, presence: true
+  validates :postage_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :amountday_id, presence: true
+  validates :price, presence: true
