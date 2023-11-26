f1d720437b68cf4d364eb2000fee6a396730788a
mamiprogramming <mamiprogramming@gmail.com>
2023-11-26 15:35:50 +0900

    修正

--- app/models/article.rb
+++
@@ -1,6 +0,0 @@
-class Article < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
-
-  validates :category_id,:situation_id,:postage_id,:prefecture_id,:amountday_id, numericality: { other_than: 1 , message: "can't be blank"}
-end
--- db/migrate/20231126033943_change_model.rb
+++
@@ -1,4 +0,0 @@
-class ChangeModel < ActiveRecord::Migration[7.0]
-  def change
-  end
-end
