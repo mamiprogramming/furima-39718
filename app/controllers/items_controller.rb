f27e4cba849ee426750488db01bae3d44c2b2c84
mamiprogramming <mamiprogramming@gmail.com>
2023-12-03 20:39:04 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,7 +50,7 @@ class ItemsController < ApplicationController
   end
 
   def contributor
-    redirect_to root_path unless current_user.id == @item.user_id
+    redirect_to root_path unless current_user == @item.user
   end
 
   def set_item
