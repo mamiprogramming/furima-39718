c1b8b32e1fa148bd0b1e9808b0cff4dcd628ef3f
mamiprogramming <mamiprogramming@gmail.com>
2023-12-10 03:13:27 +0900

    Formオブジェクト作成

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,22 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user, :item, :zip, :prefecture_id, :city, :address, :building, :telephone, :buy
+  attr_accessor :user, :item, :zip, :prefecture_id, :city, :address, :building, :telephone, :order
+
+  with_options presence: true do
+    validates :user
+    validates :item
+    validates :zip, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :telephone, numericality: { only_integer: true, message: "should be only half-width numbers" },
+                          length: { in: 10..11, message: "should be between 10 and 11 digits" },
+                          format: { with: /\A\d+\z/, message: "should contain only half-width numbers" }
+    validates :order
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(user: user, item: item)
+    Address.create(zip: zip, prefecture_id: prefecture_id, city: city, address: address, building: building, order_id: order.id)
+  end
 end
