d36bfb7709084fe636b0ae5b9f854035b99ed953
mamiprogramming <mamiprogramming@gmail.com>
2023-12-12 22:21:13 +0900

    カード決済機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,7 @@ class OrdersController < ApplicationController
   before_action :check_user, only: [:index]
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
   end
 
@@ -17,10 +18,11 @@ class OrdersController < ApplicationController
     end
   
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
-      puts @order_address.errors.full_messages
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -32,10 +34,19 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_address).permit(:zip, :prefecture_id, :city, :street_number, :building, :telephone).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_address).permit(:zip, :prefecture_id, :city, :street_number, :building, :telephone).merge(user_id: current_user.id, item_id: @item.id, token: params[:token], price: @item.price)
   end
 
   def check_user
     redirect_to root_path if user_signed_in? && @item.user_id == current_user.id
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user, :item, :zip, :prefecture_id, :city, :street_number, :building, :telephone, :order, :user_id, :item_id
+  attr_accessor :user, :item, :zip, :prefecture_id, :city, :street_number, :building, :telephone, :order, :user_id, :item_id, :token, :price
 
   with_options presence: true do
     validates :zip, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
@@ -12,6 +12,7 @@ class OrderAddress
   end
 
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :token, presence: true
 
 
   def save
