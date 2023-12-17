9cab597ab5979dc9c8dab7eafc0a650a231be1f9
mamiprogramming <mamiprogramming@gmail.com>
2023-12-17 12:07:36 +0900

    売り切れ時の編集画面遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -54,7 +54,11 @@ class ItemsController < ApplicationController
   end
 
   def contributor
-    redirect_to root_path unless current_user == @item.user
+    if @item.sold_out?
+      redirect_to root_path
+    else
+      redirect_to root_path unless current_user == @item.user
+    end
   end
 
   def set_item
